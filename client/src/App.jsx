dcbee493f6c61c570f7ae073ed046dfeb6d8105c
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-06-12 10:25:10 +0300

    Initialize TailwindCSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
+import React from "react";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,jsx,ts,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
