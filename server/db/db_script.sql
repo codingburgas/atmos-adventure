44d45b9ada2f7712f2daac2027036be23adecd6b
SHHristov20 <SHHristov20@codingburgas.bg>
2022-06-14 11:52:08 +0300

    Update DB script

--- server/db/db_script.sql
+++ server/db/db_script.sql
@@ -5,11 +5,9 @@ USE `atmos`;
 CREATE TABLE `users` (
     `uuid` VARCHAR(36) NOT NULL,
     `username` VARCHAR(16) NOT NULL,
-    `first_name` VARCHAR(50) NOT NULL,
-    `last_name` VARCHAR(50) NOT NULL,
     `email` VARCHAR(30) NOT NULL,
     `pass_hash` VARCHAR(60) NOT NULL,
     `role` VARCHAR(10) NOT NULL,
     PRIMARY KEY(`uuid`)
 );
-INSERT INTO `users` VALUES ("6706aae5-7ca0-4915-b688-098c8644407c", "admin", "Atmos", "admin", "admin@atmos.me", "$2b$10$KiqYHZJlmzl7G0fvA1XY2uYuR3QeHAA71Dn05AzfwsPcsIEeItxZS", "admin");
+INSERT INTO `users` VALUES ("6706aae5-7ca0-4915-b688-098c8644407c", "admin", "admin@atmos.me", "$2b$10$KiqYHZJlmzl7G0fvA1XY2uYuR3QeHAA71Dn05AzfwsPcsIEeItxZS", "admin");
