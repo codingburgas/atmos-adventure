76affe26fb138caff1da89271030a3cb07af5070
SHHristov20 <SHHristov20@codingburgas.bg>
2022-06-14 13:39:06 +0300

    Add check if user already exists in DB

--- server/routes/api.js
+++ server/routes/api.js
@@ -18,16 +18,43 @@ router.post('/register',(req,res)=>{
         req.body.email,
         bcrypt.hashSync(req.body.password, 10)
     ];
-    db.query('INSERT INTO users (uuid, username, email, pass_hash) VALUES (?)', [data], (err, result) => {
-        if(err) {
-            console.error(err);
-            res.send(err);
+    console.log(`User with username: "${data[1]}" trying to register.`)
+    db.query('SELECT * FROM users WHERE username = (?) OR email = (?)', [data[1], data[2]], (err, results) => {
+        if (err)
+        {
+            console.error("Something went wrong");
+            res.send("Something went wrong");
         }
-        else {
-            console.log(result); 
-            res.send(result);
+        else if (results.length > 0)
+        {
+            if(results[0].username === data[1])
+            {
+                console.log("Username already exists");
+                res.send('Username already exists');
+            }
+            else if(results[0].email === data[2])
+            {
+                console.log("Email already exists");
+                res.send('Email already exists');
+            }
+        }
+        else
+        {
+            db.query('INSERT INTO users (uuid, username, email, pass_hash) VALUES (?)', [data], (err, result) => {
+                if(err) 
+                {
+                    console.error("Something went wrong");
+                    res.send("Something went wrong");
+                }
+                else 
+                {
+                    console.log("User created"); 
+                    res.send("User created");
+                }
+            });
         }
     });
 });
+
 
 module.exports = router;
