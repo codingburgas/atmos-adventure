c5e38d942c091bd01dae5895f219214503c94ab5
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-06-15 22:47:39 +0300

    Redesign register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -26,66 +26,57 @@ const Register = () => {
   };
 
   return (
-    <div className="h-full">
-      <Navbar />
-      <div className=" bg-purple-300 laptop:h-screen bg-center bg-no-repeat bg-cover object-cover">
-        <div className="flex h-screen items-end justify-end flex-col-reverse ">
-          <div className="m-auto  bg-form bg-center bg-no-repeat bg-cover object-cover w-1/2 items-center justify-center h-4/6 rounded-3xl">
-            <div className="flex flex-col items-center bg-white w-1/2 h-full ml-auto rounded-r-3xl">
-              <h1 className="text-2xl font-serif font-bold mt-14 laptop:text-4xl laptop:mt-20">
-                Don't have an account?
-              </h1>
-              <br />
-              <form className="flex flex-col items-center justify-center mt-20 laptop:mt-20">
-                <div className="border-b-4 border-black w-full bg-transparent laptop:w-full laptop:border-b-4">
-                  <MdAccountCircle className="text-2xl inline-block laptop:text-3xl" />
-                  <input
-                    type="username"
-                    name="username"
-                    className="focus:border-none outline-none ml-3 placeholder:font-bold text-base laptop:placeholder:font-bold font-serif bg-transparent"
-                    placeholder="Username"
-                    required
-                    ref={username}
-                  />
-                  <div className="mt-1"></div>
-                </div>
-                <br />
-                <div className="border-b-4 border-black w-full bg-transparent laptop:w-full laptop:border-b-4">
-                  <MdEmail className="text-2xl inline-block laptop:text-3xl" />
-                  <input
-                    type="Email"
-                    name="username"
-                    className="focus:border-none outline-none ml-3 placeholder:font-bold text-base laptop:placeholder:font-bold font-serif bg-transparent"
-                    placeholder="Email"
-                    required
-                    ref={email}
-                  />
-                  <div className="mt-1"></div>
-                </div>
-                <div className="border-b-4 border-black w-full bg-transparent mt-5 laptop:w-full laptop:border-b-4 laptop:mt-6">
-                  <BiLockAlt className="text-2xl inline-block laptop:text-3xl" />
-                  <input
-                    type="password"
-                    name="username"
-                    className="focus:border-none outline-none ml-3 placeholder:font-bold text-base laptop:placeholder:font-bold font-serif bg-transparent"
-                    placeholder="Password"
-                    required
-                    ref={password}
-                  />
-                  <div className="mt-1"></div>
-                </div>
-                <div className="bg-blue-400 rounded-3xl text-white mt-14 laptop:mt-16 smallDesk:mt-28">
-                  <button
-                    className="font-serif font-bold text-2xl pr-24 pl-24 pt-2 pb-2 smallDesk:text-3xl smallDesk:pr-32 smallDesk:pl-32 smallDesk:pt-3 smallDesk:pb-3 laptop:text-2xl laptop:pr-32 laptop:pl-32 laptop:pt-3 laptop:pb-3"
-                    onClick={registerHandler}
-                  >
-                    Register
-                  </button>
-                </div>
-              </form>
-              <br />
-            </div>
-          </div>
+    <div className="h-screen bg-form bg-center bg-no-repeat bg-cover object-cover">
+      <div className="h-full w-full backdrop-blur-sm desktop:w-half extra:backdrop-blur-md">
+        <div className="flex flex-col">
+          <h1 className="font-dreamScape text-7xl flex flex-col items-center justify-center relative pt-20 text-white extra:text-9xl">
+            ATMOS
+          </h1>
+          <h1 className="font-dreamScapeSans flex flex-col items-center justify-center relative text-lightGreen text-2xl extra:text-4xl">
+            Sign up!
+          </h1>
+        </div>
+
+        <div className="flex flex-row items-center justify-center pt-10 ">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="bg-account bg-no-repeat font-bold [background-position-x:1%] [background-position-y:48%] bg-45 w-9/12 py-5 px-14 text-lg extra:w-8/12 extra:py-6 extra:bg-70 extra:[background-position-x:-1%] extra:[background-position-y:60%] extra:placeholder:text-2xl extra:text-2xl leading-tight appearance-none box-border rounded-xl backdrop-blur-sm bg-darkBlue z-0 text-white placeholder:font-bold placeholder:text-lg placeholder:text-white focus:outline-none"
+            placeholder="Username"
+            ref={username}
+            required
+          />
+        </div>
+        <div className="flex flex-row items-center justify-center pt-10 ">
+          <input
+            type="password"
+            name=""
+            id=""
+            className="bg-lock bg-no-repeat font-bold [background-position-x:1%] [background-position-y:48%] bg-45 w-9/12 py-5 px-14 text-lg extra:w-8/12 extra:py-6 extra:bg-70 extra:[background-position-x:-1%] extra:[background-position-y:60%] extra:placeholder:text-2xl extra:text-2xl leading-tight appearance-none box-border rounded-xl backdrop-blur-sm bg-darkBlue z-0 text-white placeholder:font-bold placeholder:text-lg placeholder:text-white focus:outline-none"
+            placeholder="Password"
+            ref={password}
+            required
+          />
+        </div>
+        <div className="flex flex-row items-center justify-center pt-10 ">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="bg-email bg-no-repeat font-bold [background-position-x:1%] [background-position-y:48%] bg-45 w-9/12 py-5 px-14 text-lg extra:w-8/12 extra:py-6 extra:bg-70 extra:[background-position-x:-1%] extra:[background-position-y:60%] extra:placeholder:text-2xl extra:text-2xl leading-tight appearance-none box-border rounded-xl backdrop-blur-sm bg-darkBlue z-0 text-white placeholder:font-bold placeholder:text-lg placeholder:text-white focus:outline-none"
+            placeholder="Email"
+            ref={email}
+            required
+          />
+        </div>
+        <div className="flex flex-row items-center justify-center pt-16 extra:pt-20 ">
+          <button
+            className="py-5 px-28 font-sans bg-darkBlue rounded-full extra:px-32 text-white font-bold text-xl extra:text-3xl"
+            onClick={registerHandler}
+          >
+            Sign up
+          </button>
         </div>
       </div>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,23 +4,41 @@ module.exports = {
   purge: ["src/**/*.js", "src/**/*.jsx"],
   theme: {
     screens: {
-      smallDesk: "1746px",
-      laptop: "1537px",
-      smallLaptop: "1281px",
-      bigTablet: "1098px",
-      tablet: "907px",
+      extra: "1281px",
+      desktop: "1023px",
     },
     themeVariants: ["dust", "nuclea"],
     extend: {
       fontFamily: {
         sans: ["Commissioner", ...defaultTheme.fontFamily.sans],
         serif: ["Inter", ...defaultTheme.fontFamily.serif],
+        dreamScape: ["Dreamscape", ...defaultTheme.fontFamily.sans],
+        dreamScapeSans: ["Dreamscape Sans", ...defaultTheme.fontFamily.sans],
+      },
+      width: {
+        half: "38%",
       },
       backgroundImage: {
         dust: "url('./assets/tempDust.svg')",
         nuclea: "url('./assets/tempNuclea.svg')",
-        form: "url('./assets/formBackground.svg')",
+        form: "url('./assets/formBackground.jpg')",
+        account:
+          "url('https://cdn.discordapp.com/attachments/883768694835130390/986689684820357170/My_project_2.png')",
+        lock: "url('https://cdn.discordapp.com/attachments/883768694835130390/986678843786035261/My_project.png')",
+        email:
+          "url('https://cdn.discordapp.com/attachments/883768694835130390/986687019046551642/My_project_1.png')",
       },
+      backgroundSize: {
+        20: "27px",
+        45: "45px",
+        70: "70px",
+      },
+    },
+    colors: {
+      darkBlue: "#39575D",
+      white: "#FFFFFF",
+      lightGreen: "#DAF5D5",
+      lightBlue: "#7D7BDC",
     },
   },
   variants: {
