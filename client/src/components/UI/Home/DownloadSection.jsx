d1fd7b303f04f698287e18b3be91a4a89ec5b4b2
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-06-23 18:07:16 +0300

    Create download section for the home page
    Co-authored-by: Valeri Ivanov <vtivanov20@codingburgas.bg>

--- client/src/components/UI/Home/DownloadSection.jsx
+++ client/src/components/UI/Home/DownloadSection.jsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const DownloadSection = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className="h-screen bg-[url('https://i.imgur.com/if55Cdl.png')] bg-cover mt-20">
-      <div className="flex flex-row justify-center items-center pt-20 text-white font-sans font-semibold text-4xl">
+    <div className="h-full bg-[url('https://i.imgur.com/XlUu24Q.png')] tablet:bg-[url('https://i.imgur.com/8cn2eEF.png')] bg-cover bg-no-repeat object-cover mt-20">
+      <div
+        className="flex flex-row justify-center items-center pt-20 text-white font-sans font-semibold text-4xl desktop:pt-60 desktop:text-6xl el:!text-8xl"
+        data-aos="fade-down"
+        data-aos-duration="1500"
+      >
         You on board?
       </div>
-      <div className="flex flex-col justify-center items-center h-[80vh]">
+      <div
+        className="flex flex-col justify-center items-center h-[80vh] mt-20 desktop:mt-0"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+      >
         <button className="bg-button outline outline-[.0001px] text-white outline-white px-10 py-4 rounded-full font-sans font-bold text-2xl">
           DOWNLOAD
         </button>
