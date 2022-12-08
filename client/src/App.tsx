afda4e1191dea47b5eb9bad17023823a005c82a7
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-12-08 19:30:36 +0200

    Recreate client folder with react TS

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-center">Hello World</div>;
+}
+
+export default App;
--- client/src/components/UI/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="flex flex-row items-center justify-center h-screen">
-      <div className="animate-spin border-solid border-[16px] rounded-full border-[#f3f3f3] border-t-[#3498db] w-[120px] h-[120px]"></div>
-    </div>
-  );
-};
-
-export default LoadingSpinner;
